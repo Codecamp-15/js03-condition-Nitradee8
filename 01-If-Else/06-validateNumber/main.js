6c1e4ee2f40aecf33df20f81e97487fd31ddff09
Nitradee8 <nbtt.nik@gmail.com>
2023-08-07 20:51:03 +0700

    lab6

---
+++ 01-If-Else/06-validateNumber/main.js
@@ -0,0 +1,14 @@
+let input1 = prompt('กรุณากรอกตัวเลข');
+let input2 = prompt('กรุณากรอกตัวเลข');
+
+
+// if (num1 == null || num1.trim() === '') {
+//     alert('Invalid number')
+// }
+
+
+if (true ) {
+    alert(Number(input1) + Number(input2))
+}   
+
+
