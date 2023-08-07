a20141a94526dff18d5bd728cd9bfff333f21d74
Nitradee8 <nbtt.nik@gmail.com>
2023-08-07 15:29:26 +0700

    lab4

---
+++ 01-If-Else/04-checkNumber/main.js
@@ -0,0 +1,28 @@
+let num1 = prompt('กรุณากรอกตัวเลข');
+
+if (num1 > 0) {
+    alert('Positive number')
+} else if (num1 == 0) {
+    alert('Zero')
+} else if (num1 < 0) {
+     alert('Negative number')
+} else {
+    alert('Invalid number')  
+}
+
+
+// if (num1 == null || num1.trim() === '') {
+//     alert('Invalid number')
+// }
+
+// if (isNaN(num1)) {
+//     alert('Invalid number')
+// }
+
+// if (num1 === null || num1.trim() === '' || isNaN(num1)) {
+//     alert('Invalid number')
+// }
+
+
+
+
