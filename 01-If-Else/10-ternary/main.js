9c52541c1304ef659b82f9e6bc498361a590fcf8
Nitradee8 <nbtt.nik@gmail.com>
2023-08-07 22:22:42 +0700

    lab10

---
+++ 01-If-Else/10-ternary/main.js
@@ -0,0 +1,14 @@
+// เปลี่ยน if-else ข้างล่างให้อยู่ในรูปของ Ternary Operators
+
+// let age = prompt("How old are you");
+// let message;
+// if (age >= 18) {
+//   message = "Allowed";
+// } else {
+//   message = "Not allowed";
+// }
+
+console.log("How old are you");
+let message = age >= 18 ? "Allowed": "Not allowed";
+console.log("Allowed: " + "Not allowed" );
+
